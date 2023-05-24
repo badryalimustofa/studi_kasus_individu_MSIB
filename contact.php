52cccbb39f29869d750068d28fa4588a87e20e90
badryalimustofa <125412863+badryalimustofa@users.noreply.github.com>
2023-05-24 15:51:31 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,39 @@
+    <h1>Contact Us</h1>
+    <div class="content_half float_l">
+        <p>If you have trouble or any question more about us, please contact us</p>
+        <div id="contact_form">
+            <form method="post" name="contact" action="#">
+
+                <label for="author">Name:</label> <input type="text" id="author" name="author"
+                    class="required input_field" />
+                <div class="cleaner h10"></div>
+                <label for="email">Email:</label> <input type="text" id="email" name="email"
+                    class="validate-email required input_field" />
+                <div class="cleaner h10"></div>
+
+                <label for="phone">Phone:</label> <input type="text" name="phone" id="phone" class="input_field" />
+                <div class="cleaner h10"></div>
+
+                <label for="text">Message:</label> <textarea id="text" name="text" rows="0" cols="0"
+                    class="required"></textarea>
+                <div class="cleaner h10"></div>
+
+                <input type="submit" class="submit_btn" name="submit" id="submit" value="Send" />
+
+            </form>
+        </div>
+    </div>
+    <div class="content_half float_r">
+        <h5>Our Office</h5>
+        Gg. Duku II B, RT.03/RW.04, Jajar, Kec. Laweyan,<br />
+        Kota Surakarta, Jawa Tengah 57144
+
+
+        Phone: 0852 9384 0367<br />
+        Email: <a href="alimustofa072000@gmail.com">alimustofa072000@gmail.com</a><br />
+    </div>
+
+    <div class="cleaner h40"></div>
+    <!--map pada peta-->
+    <iframe width="680" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+        src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Central+Park,+New+York,+NY,+USA&amp;aq=0&amp;sll=14.093957,1.318359&amp;sspn=69.699334,135.263672&amp;vpsrc=6&amp;ie=UTF8&amp;hq=Central+Park,+New+York,+NY,+USA&amp;ll=40.778265,-73.96988&amp;spn=0.033797,0.06403&amp;t=m&amp;output=embed"></iframe>
